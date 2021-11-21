d9af2781bb37c4e9f218fef3a3f36a01948ec5aa
Jason A Leo <30601823+xezian@users.noreply.github.com>
2021-11-21 10:37:20 -0500

    Refines blog importer script, tests on first 5 blog posts

--- scripts/import_blog.py
+++ scripts/import_blog.py
@@ -1,4 +1,8 @@
 import os
+from os.path import join, dirname
+import requests
+from datetime import datetime
+from dotenv import load_dotenv
 from pyquery import PyQuery as pq
 import cloudinary
 import wpparser
@@ -7,37 +11,73 @@ import cloudinary.api
 
 from pprint import pprint
 
-def upload_image(image):
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+cloud = cloudinary
+cloud.config(
+    cloud_name = os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    api_key = os.environ.get('CLOUDINARY_API_KEY'),
+    api_secret = os.environ.get('CLOUDINARY_API_SECRET'),
+    secure = True
+)
+
+def upload_image(cloud, image):
     """
     Upload the image to Cloudinary
     """
-
+    # Download the image
+    image_url = image.get('src')
+    filename = image_url.split("/")[-1]
+    r = requests.get(image_url, stream = True)
+    r.raw.decode_content = True
+    with open(filename, 'wb') as file:
+        for chunk in r.iter_content(chunk_size = 1024):
+            if chunk:
+                file.write(chunk)
     # Upload the image
-    cloudinary.uploader.upload(image)
-    image.src = cloudinary.utils.cloudinary_url()
+    response = cloud.uploader.upload(filename, public_id = filename)
+    pq(image).attr('src', response.get('secure_url'))
+    # Delete the file
+    os.remove(filename)
 
 
-def create_markdown_file(title, content, image, tags):
+def create_markdown_file(cloud, post):
     """
     Create the Markdown file
     """
+    # Create /_posts directory if it doesn't exist
+    if not os.path.exists('_posts'):
+        os.makedirs('_posts')
     # Create new markdown file
-    md_file = os.path.join(os.getcwd(), '_posts', '{}.md'.format(title))
+    md_file = join(os.getcwd(), '_posts', f'{post["slug"]}.md')
 
-    if content and "http://c-t-l.org/wp-content/uploads/" in content:
-        doc = pq(content)
-        doc('img').each(lambda i, e: upload_image(e.get('src')))
+    # pq is pyquery (jquery like html parsing)
+    if post["content"]and "http://c-t-l.org/wp-content/uploads/" in post["content"]:
+        doc = pq(post["content"])
+        # doc('img').each(lambda i, e: upload_image(cloud, e))
+        for image in doc('img'):
+            upload_image(cloud, image)
+        post["content"] = doc.html()
     
-    # Create the file
-    with open(md_file, 'w') as f:
+    # Create the file like:
+    # ---
+    # slug: /middle-school-book-blog/is-this-about-something
+    # date: 2021-09-25T14:20:29.338Z
+    # title: Is this about something?
+    # blog: middle-school-book-blog
+    # ---
+    with open(md_file, 'w') as file:
+        file.write(f'---\n')
+        file.write(f'slug: /middle-school-book-blog/{post["slug"]}\n')
         # Write the title
-        f.write('# {}\n'.format(title))
+        file.write(f'date: {post["date"]}\n')
+        file.write(f'title: {post["title"]}\n')
+        file.write(f'blog: middle-school-book-blog\n')
+        file.write(f'---\n\n')
         # Write the content
-        f.write('{}\n'.format(content))
-        # Write the tags
-        f.write('Tags: {}\n'.format(tags))
-        # Write the image
-        f.write('![{}]({})\n'.format(image, image))
+        file.write(f'{post["content"]}\n')
 
 
 def main():
@@ -46,26 +86,28 @@ def main():
     with images uploaded to Cloudinary
     """
     # Get the XML file
-    xml_file = os.path.join(os.getcwd(), 'bookblog.xml')
+    xml_file = join(os.getcwd(), 'bookblog.xml')
     # Parse the XML file
     data = wpparser.parse(xml_file)
     posts = data['posts']
 
-    posts = [p for p in posts if p['content'] is not None]
-    print(len(posts))
+    posts = [p for p in posts if p['post_type'] == 'post' and p['content'] is not None and p['title'] is not None]
 
     # Iterate over the posts
-    for post in []:
-        # Get the title
-        title = post.get('title')
-        # Get the content
-        content = post.get('content')
-        # Get the image
-        image = post.get('image')
-        # Get the tags
-        tags = post.get('tags')
+    for post in posts[:5]:
+        normalized_post = {
+            'title': post.get('title'),
+            'content': post.get('content'),
+            'categories': post.get('categories'),
+            'comments': post.get('comments'),
+            'creator': post.get('creator'),
+            'slug': post.get('post_name'),
+            'date': datetime.strptime(post.get('post_date'), '%Y-%m-%d %H:%M:%S').isoformat() + 'Z',
+            'tags': post.get('tags')
+        }
         # Create the Markdown file
-        create_markdown_file(title, content, image, tags)
+        create_markdown_file(cloud, normalized_post)
+
 
 
 if __name__ == "__main__":
---
+++ src/markdown/blogs/middle-school-book-blog/house-of-the-scorpion-by-nancy-farmer.md
@@ -0,0 +1,26 @@
+---
+slug: /middle-school-book-blog/house-of-the-scorpion-by-nancy-farmer
+date: 2009-05-07T17:31:00Z
+title: House of the Scorpion, by Nancy Farmer
+blog: middle-school-book-blog
+---
+
+<img class="alignleft size-full wp-image-97" title="housescorpion" src="https://res.cloudinary.com/center-for-teaching-learning/image/upload/v1637506535/housescorpion.jpg.jpg" alt="housescorpion" width="100" height="150"/>The book starts out with Matt, the main character, living in a small shack in the middle of a large field. A group of kids find him; he breaks out of the shack and joins them. When he cuts his foot, the kids take him to a mansion owned by an important man named El Patron. We soon find out that Matt is actually a clone of El Patron, then follow him on his adventures toward the truth.
+
+The setting is far in the future, when chips can be implanted in people and animals to make them obey any command given, cloning happens frequently, and the border between the U.S. and Mexico is opium-growing land.
+
+<em> </em>
+
+<em>House of the Scorpion</em> deals with several themes you don't find in many other young adult novels: cloning, slavery, animal cruelty, and species-ism. Species-ism is like racism, but with different species of humans instead of races.
+
+Matt is one of the best-developed characters I have ever met in fiction. Farmer had an advantage here because the story takes place in a completely alien world of her own invention. The reader knows nothing other than what Matt knows. This makes every situation intriguing.
+
+Some young adult novels that are similar in genre to <em>House of the Scorpion </em>are <em>Rash</em> by Pete Hautman and <em>Feed </em>by M.T. Anderson. These are also very good books, also set in a dystopian future but with very different themes.
+
+I rated <em>House of the Scorpion</em> a 10 out of 10. I would highly recommend it to any reader.
+
+Daniel
+
+Publisher:  Atheneum Books, 380 pages
+
+<a href="http://www.amazon.com/gp/product/0689852231">Read more about this title on Amazon.com</a>
