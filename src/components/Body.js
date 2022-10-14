13e0baa6f4aa604e2525ca832c2722755a27c54f
Jason A Leo <30601823+xezian@users.noreply.github.com>
2022-10-14 14:55:35 -0400

    Move function into component

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 
-// utils
-import { enwrapImgTagsInAnotherDivWithClass } from "../utils";
+const enwrapImgTagsInAnotherDivWithClass = (htmlToEnwrap, className) => {
+  const parser = new DOMParser();
+  const document = parser.parseFromString(htmlToEnwrap, "text/html");
+  const imgs = document.querySelectorAll("img");
+  imgs.forEach((img) => {
+    const div = document.createElement("div");
+    div.classList.add(className);
+    img.parentNode.insertBefore(div, img);
+    div.appendChild(img);
+  });
+  return document.body.innerHTML;
+};
 
 const BodyDiv = styled.div`
   width: 100%;
