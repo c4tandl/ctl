b2dc077fa6f80099dda1c117ff34f77bec0746ea
Jason A Leo <30601823+xezian@users.noreply.github.com>
2023-02-26 11:53:35 -0500

    refactors svgs to export styled components from assets

--- src/assets/svgs/drawings/index.js
+++ src/assets/svgs/drawings/index.js
@@ -3,12 +3,12 @@ import styled from "styled-components";
 
 import BirdSVG from "./bird.svg";
 import CTLAcornSVG from "./ctl.acorn.svg";
-// import "./acorn.person.1.svg";
-// import "./acorn.4.svg";
-// import "./acorn.person.2.svg";
-// import "./acorn.worm.svg";
-// import "./happy.acorn.3.svg";
-// import "./sun.svg";
+import AcornPerson1SVG from "./acorn.person.1.svg";
+import AcornSVG from "./acorn.4.svg";
+import AcornPerson2SVG from "./acorn.person.2.svg";
+import WormAcornSVG from "./acorn.worm.svg";
+import HappyAcornSVG from "./happy.acorn.3.svg";
+import SunSVG from "./sun.svg";
 
 const BirdDiv = styled.div`
   width: 200px;
@@ -65,130 +65,151 @@ export const CTLAcorn = ({ svgProps, ...props }) => {
   );
 };
 
-// const MidBot = styled.div`
-//   grid-area: acorns;
-//   width: 100%;
-//   height: 100%;
-//   display: grid;
-//   margin-top: -100px;
-//   margin-left: 100px;
-//   grid-template-areas:
-//     "top-left .       ."
-//     "mid-left .       ."
-//     "bot-left bot-mid .";
-//   svg {
-//     width: 150px;
-//     height: 150px;
-//   }
-//   svg.acorn {
-//     grid-area: bot-left;
-//     .st0 {
-//       fill: #a97c50;
-//     }
-//     .st1 {
-//       fill: #3c2415;
-//     }
-//     .st2 {
-//       fill: #754c29;
-//     }
-//     .st3 {
-//       fill: #603913;
-//     }
-//   }
-//   svg.person1 {
-//     grid-area: bot-mid;
-//     .st0 {
-//       fill: #8dc63f;
-//     }
-//     .st1 {
-//       fill: #ffffff;
-//     }
-//   }
-//   svg.person2 {
-//     grid-area: top-left;
-//     margin-bottom: -65px;
-//     .st0 {
-//       fill: #a97c50;
-//     }
-//     .st1 {
-//       fill: #ffffff;
-//     }
-//     .st2 {
-//       fill: #603913;
-//     }
-//   }
-//   svg.happy {
-//     grid-area: mid-left;
-//     .st0 {
-//       fill: #ffffff;
-//     }
-//     .st1 {
-//       fill: #d7df23;
-//     }
-//     .st2 {
-//       fill: #754c29;
-//     }
-//     .st3 {
-//       fill: #a97c50;
-//     }
-//   }
-// `;
+const FourAcornsDiv = styled.div`
+  width: 100%;
+  height: 100%;
+  display: grid;
+  margin-top: -100px;
+  margin-left: 100px;
+  grid-template-areas:
+    "top-left .       ."
+    "mid-left .       ."
+    "bot-left bot-mid .";
+  svg {
+    width: 150px;
+    height: 150px;
+  }
+  svg.acorn {
+    grid-area: bot-left;
+    .st0 {
+      fill: #a97c50;
+    }
+    .st1 {
+      fill: #3c2415;
+    }
+    .st2 {
+      fill: #754c29;
+    }
+    .st3 {
+      fill: #603913;
+    }
+  }
+  svg.person1 {
+    grid-area: bot-mid;
+    .st0 {
+      fill: #8dc63f;
+    }
+    .st1 {
+      fill: #ffffff;
+    }
+  }
+  svg.person2 {
+    grid-area: top-left;
+    margin-bottom: -65px;
+    .st0 {
+      fill: #a97c50;
+    }
+    .st1 {
+      fill: #ffffff;
+    }
+    .st2 {
+      fill: #603913;
+    }
+  }
+  svg.happy {
+    grid-area: mid-left;
+    .st0 {
+      fill: #ffffff;
+    }
+    .st1 {
+      fill: #d7df23;
+    }
+    .st2 {
+      fill: #754c29;
+    }
+    .st3 {
+      fill: #a97c50;
+    }
+  }
+`;
+export const FourAcorns = ({ svgProps, ...props }) => {
+  return (
+    <FourAcornsDiv {...props}>
+      <AcornSVG className="acorn" />
+      <AcornPerson1SVG className="person1" />
+      <AcornPerson2SVG className="person2" />
+      <HappyAcornSVG className="happy" />
+    </FourAcornsDiv>
+  );
+};
 
-// const SunArea = styled.div`
-//   grid-area: sun;
-//   width: 400px;
-//   height: 400px;
-//   svg {
-//     .st0 {
-//       fill: #fbb040;
-//     }
-//     .st1 {
-//       fill: #ffffff;
-//     }
-//   }
-// `;
+const SunDiv = styled.div`
+  width: 400px;
+  height: 400px;
+  svg {
+    .st0 {
+      fill: #fbb040;
+    }
+    .st1 {
+      fill: #ffffff;
+    }
+  }
+`;
+export const Sun = ({ svgProps, ...props }) => {
+  return (
+    <SunDiv {...props}>
+      <SunSVG {...svgProps} />
+    </SunDiv>
+  );
+};
 
-// const WormArea = styled.div`
-//   grid-area: worm;
-//   svg {
-//     width: 200px;
-//     height: 200px;
-//     margin-bottom: -305px;
-//     .st0 {
-//       fill: #ef4136;
-//     }
-//     .st1 {
-//       fill: #1c75bc;
-//     }
-//     .st2 {
-//       fill: #ec008c;
-//     }
-//     .st3 {
-//       fill: #00a79d;
-//     }
-//     .st4 {
-//       fill: #92278f;
-//     }
-//     .st5 {
-//       fill: #2b3990;
-//     }
-//     .st6 {
-//       fill: #f7941d;
-//     }
-//     .st7 {
-//       fill: #009444;
-//     }
-//     .st8 {
-//       fill: #f9ed32;
-//     }
-//     .st9 {
-//       fill: #8dc63f;
-//     }
-//     .st10 {
-//       fill: #ffffff;
-//     }
-//     .st11 {
-//       fill: #39b54a;
-//     }
-//   }
-// `;
+const WormAcornDiv = styled.div`
+  svg {
+    width: 200px;
+    height: 200px;
+    margin-bottom: -305px;
+    .st0 {
+      fill: #ef4136;
+    }
+    .st1 {
+      fill: #1c75bc;
+    }
+    .st2 {
+      fill: #ec008c;
+    }
+    .st3 {
+      fill: #00a79d;
+    }
+    .st4 {
+      fill: #92278f;
+    }
+    .st5 {
+      fill: #2b3990;
+    }
+    .st6 {
+      fill: #f7941d;
+    }
+    .st7 {
+      fill: #009444;
+    }
+    .st8 {
+      fill: #f9ed32;
+    }
+    .st9 {
+      fill: #8dc63f;
+    }
+    .st10 {
+      fill: #ffffff;
+    }
+    .st11 {
+      fill: #39b54a;
+    }
+  }
+`;
+export const WormAcorn = ({ svgProps, ...props }) => {
+  return (
+    <WormAcornDiv {...props}>
+      <WormAcornSVG {...svgProps} />
+    </WormAcornDiv>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,15 +8,13 @@ import Carousel from "../components/Carousel";
 import Body from "../components/Body";
 
 // svgs
-
-import { Bird, CTLAcorn } from "../assets/svgs/drawings";
-import Sun from "../assets/svgs/drawings/sun.svg";
-import HappyAcorn from "../assets/svgs/drawings/happy.acorn.3.svg";
-import Acorn from "../assets/svgs/drawings/acorn.4.svg";
-// import Bird from "../assets/svgs/drawings/bird.svg";
-import AcornWorm from "../assets/svgs/drawings/acorn.worm.svg";
-import AcornPerson1 from "../assets/svgs/drawings/acorn.person.1.svg";
-import AcornPerson2 from "../assets/svgs/drawings/acorn.person.2.svg";
+import {
+  Bird,
+  CTLAcorn,
+  Sun,
+  FourAcorns,
+  WormAcorn,
+} from "../assets/svgs/drawings";
 
 const Page = styled.div`
   display: flex;
@@ -54,6 +52,7 @@ const BodyHolder = styled.div`
 `;
 
 const FullPage = styled.div`
+  position: relative;
   display: grid;
   width: calc(100vw - 20px);
   margin-top: ${(props) => (props.coverSlideshow ? "0" : "250px")};
@@ -91,134 +90,6 @@ const HideOnSmallscreen = styled.div`
   }
 `;
 
-const MidBot = styled.div`
-  grid-area: acorns;
-  width: 100%;
-  height: 100%;
-  display: grid;
-  margin-top: -100px;
-  margin-left: 100px;
-  grid-template-areas:
-    "top-left .       ."
-    "mid-left .       ."
-    "bot-left bot-mid .";
-  svg {
-    width: 150px;
-    height: 150px;
-  }
-  svg.acorn {
-    grid-area: bot-left;
-    .st0 {
-      fill: #a97c50;
-    }
-    .st1 {
-      fill: #3c2415;
-    }
-    .st2 {
-      fill: #754c29;
-    }
-    .st3 {
-      fill: #603913;
-    }
-  }
-  svg.person1 {
-    grid-area: bot-mid;
-    .st0 {
-      fill: #8dc63f;
-    }
-    .st1 {
-      fill: #ffffff;
-    }
-  }
-  svg.person2 {
-    grid-area: top-left;
-    margin-bottom: -65px;
-    .st0 {
-      fill: #a97c50;
-    }
-    .st1 {
-      fill: #ffffff;
-    }
-    .st2 {
-      fill: #603913;
-    }
-  }
-  svg.happy {
-    grid-area: mid-left;
-    .st0 {
-      fill: #ffffff;
-    }
-    .st1 {
-      fill: #d7df23;
-    }
-    .st2 {
-      fill: #754c29;
-    }
-    .st3 {
-      fill: #a97c50;
-    }
-  }
-`;
-
-const SunArea = styled.div`
-  grid-area: sun;
-  width: 400px;
-  height: 400px;
-  svg {
-    .st0 {
-      fill: #fbb040;
-    }
-    .st1 {
-      fill: #ffffff;
-    }
-  }
-`;
-
-const WormArea = styled.div`
-  grid-area: worm;
-  svg {
-    width: 200px;
-    height: 200px;
-    margin-bottom: -305px;
-    .st0 {
-      fill: #ef4136;
-    }
-    .st1 {
-      fill: #1c75bc;
-    }
-    .st2 {
-      fill: #ec008c;
-    }
-    .st3 {
-      fill: #00a79d;
-    }
-    .st4 {
-      fill: #92278f;
-    }
-    .st5 {
-      fill: #2b3990;
-    }
-    .st6 {
-      fill: #f7941d;
-    }
-    .st7 {
-      fill: #009444;
-    }
-    .st8 {
-      fill: #f9ed32;
-    }
-    .st9 {
-      fill: #8dc63f;
-    }
-    .st10 {
-      fill: #ffffff;
-    }
-    .st11 {
-      fill: #39b54a;
-    }
-  }
-`;
-
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
 }) {
@@ -241,28 +112,17 @@ export default function Template({
           <Body body={html} />
         </BodyHolder>
         <HideOnSmallscreen>
-          <SunArea>
-            <Sun />
-          </SunArea>
-          {/* <BirdArea> */}
+          <Sun style={{ gridArea: "sun" }} />
           <Bird
             style={{ gridArea: "bird" }}
             svgProps={{ transform: "scale(-1 1)" }}
           />
-          {/* </BirdArea> */}
           <CTLAcorn
             style={{ gridArea: "ctl" }}
             svgProps={{ style: { marginBottom: "-290px" } }}
           />
-          <MidBot>
-            <Acorn className="acorn" />
-            <AcornPerson1 className="person1" />
-            <AcornPerson2 className="person2" />
-            <HappyAcorn className="happy" />
-          </MidBot>
-          <WormArea>
-            <AcornWorm />
-          </WormArea>
+          <FourAcorns style={{ gridArea: "acorns" }} />
+          <WormAcorn style={{ gridArea: "worm" }} />
         </HideOnSmallscreen>
       </FullPage>
       <Updated title={`Last updated - ${date}`}>&Delta;</Updated>
