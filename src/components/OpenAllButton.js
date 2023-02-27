5c495dc574b59f099b2f3efc4f17be9e0d94caeb
Jason A Leo <30601823+xezian@users.noreply.github.com>
2023-02-27 11:43:24 -0500

    Moves the openall closeall button to the better placement

--- src/components/OpenAllButton.js
+++ src/components/OpenAllButton.js
@@ -4,11 +4,47 @@ import styled from "styled-components";
 import Plus from "../assets/svgs/icons/plus.svg";
 import Minus from "../assets/svgs/icons/minus.svg";
 
+const OpenCloseButtonArea = styled.div`
+  position: fixed;
+  top: 116px;
+  z-index: 10;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  @media only screen and (max-width: 1200px) {
+    top: 103px;
+  }
+  @media only screen and (max-width: 1160px) {
+    top: 146px;
+  }
+  @media only screen and (max-width: 900px) {
+    top: 87px;
+  }
+  @media only screen and (max-width: 777px) {
+    top: 75px;
+  }
+  @media only screen and (max-width: 550px) {
+    top: 72px;
+  }
+`;
+
+const LittleWiderThanNav = styled.div`
+  display: flex;
+  width: 999px;
+  justify-content: flex-end;
+  @media only screen and (max-width: 1160px) {
+    width: 160px;
+  }
+  @media only screen and (max-width: 1025px) {
+    width: 265px;
+  }
+  @media only screen and (max-width: 900px) {
+    width: calc(100vw - 24px);
+  }
+`;
+
 const OpenCloseButton = styled.button`
   z-index: 2;
-  position: fixed;
-  top: 166px;
-  right: 30vw;
   color: black;
   border: 0;
   padding: 0;
@@ -28,12 +64,16 @@ const OpenCloseButton = styled.button`
 const OpenAllButton = (props) => {
   const { open, onClick } = props;
   return (
-    <OpenCloseButton
-      title={`${open ? "Close" : "Open"} all sections`}
-      onClick={onClick}
-    >
-      {open ? <Minus /> : <Plus />}
-    </OpenCloseButton>
+    <OpenCloseButtonArea>
+      <LittleWiderThanNav>
+        <OpenCloseButton
+          title={`${open ? "Close" : "Open"} all sections`}
+          onClick={onClick}
+        >
+          {open ? <Minus /> : <Plus />}
+        </OpenCloseButton>
+      </LittleWiderThanNav>
+    </OpenCloseButtonArea>
   );
 };
 
