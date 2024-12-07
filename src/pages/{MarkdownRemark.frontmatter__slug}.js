8ba3a83112364a6f09d97f8a299c26a7e3b55e2b
Jason A Leo <30601823+xezian@users.noreply.github.com>
2024-12-07 13:02:21 -0500

    Format blog posts a little bit

--- src/pages/{MarkdownRemark.frontmatter__slug}.js
+++ src/pages/{MarkdownRemark.frontmatter__slug}.js
@@ -1,6 +1,22 @@
 import React from "react";
 import { graphql, Link } from "gatsby";
 import { authorsMap, categoriesMap } from "../assets/blogmaps";
+import styled from "styled-components";
+
+const MainContainer = styled.div`
+  margin: 0 5vw;
+`
+const Header = styled.div`
+  margin: 20px 0;
+  font-family: "URWDIN-Medium", "Helvetica Neue", Arial, Helvetica, sans-serif;
+`
+
+const Body = styled.div`
+  ul {
+    margin-left: 30px;
+    list-style: disc;
+  }
+`
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
@@ -8,25 +24,31 @@ export default function Template({
   const { markdownRemark } = data; // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark;
   return (
-    <div>
+    <MainContainer>
       <div>
         <Link to={`/${frontmatter.blog}`}>All Posts</Link>
       </div>
-      <div>
-        <div>
-          <h2>{frontmatter.title}</h2>
-          <h2>{frontmatter.date}</h2>
+      <Header>
+        <h2>{frontmatter.title}</h2>
+        <h2>{frontmatter.date}</h2>
+        {
+          frontmatter.categories ? (
           <h2>
             categories:{" "}
             {frontmatter.categories?.map((x) => categoriesMap[x]).join(", ")}
           </h2>
+          ) : null
+        }
+        {
+          frontmatter.authors ? (
           <h2>
             authors: {frontmatter.authors?.map((x) => authorsMap[x]).join(", ")}
           </h2>
-        </div>
-        <div dangerouslySetInnerHTML={{ __html: html }} />
-      </div>
-    </div>
+          ) : null
+        }
+      </Header>
+      <Body dangerouslySetInnerHTML={{ __html: html }} />
+    </MainContainer>
   );
 }
 
