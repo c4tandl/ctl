8a6f76338fb67922d458f0033c7889d9fd9add55
Jason A Leo <30601823+xezian@users.noreply.github.com>
2025-10-06 18:24:31 -0400

    Remove gatsby-plugin-netlify too

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,6 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
     "gatsby-plugin-layout",
-    "gatsby-plugin-netlify",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
