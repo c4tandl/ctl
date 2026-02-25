4fd91fa9bc35245c9672bd81099fd64de22c9184
Jason A. Leo <30601823+xezian@users.noreply.github.com>
2026-02-25 14:14:51 -0700

    Update cms.js

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -17,7 +17,7 @@ init({
     backend: {
       name: "github",
       repo: "c4tandl/ctl",
-      branch: "delay-and-closing-page-under-contact-us",
+      branch: "main",
     },
     load_config_file: false,
     media_library: {
